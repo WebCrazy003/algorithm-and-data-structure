91cb2172c164253cabc193beec916d7c1c60b8c4
WebCrazy003 <RayCoaster99001@gmail.com>
2023-06-17 22:09:40 +0300

    updates

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,54 @@
-'use strict';
+"use strict";
 
-class LinkedList {
-
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
-module.exports = LinkedList;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+  includes(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.data === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let currentNode = this.head;
+    let result = "";
+    while (currentNode) {
+      result += `{ ${currentNode.data} } -> `;
+      currentNode = currentNode.next;
+    }
+    result += "NULL";
+    return result;
+  }
+}
+
+const ll = new LinkedList();
+ll.insert("a");
+ll.insert("b");
+ll.insert("c");
+
+console.log(ll.toString()); // "{ c } -> { b } -> { a } -> NULL"
+
+module.exports = { LinkedList, Node };
