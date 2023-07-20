503241589333a38a4b20dd8f6b0c1e5e276bfac9
WebCrazy003 <RayCoaster99001@gmail.com>
2023-07-20 22:37:11 +0300

    tree-max

--- javascript/trees/index.js
+++ javascript/trees/index.js
@@ -1,24 +1,13 @@
 "use strict";
 
-const Node = require("./binary-tree/Node");
-const BinarySearchTree = require("./binary-tree/BinaryTree");
+const BT = require("./binary-tree/BinaryTree");
 
-let tree = new BinarySearchTree();
+const tree = new BT();
 
-// Add nodes to the binary search tree
-tree.add(5);
-tree.add(3);
-tree.add(7);
-tree.add(2);
-tree.add(4);
-tree.add(6);
-tree.add(8);
+tree.Add(2);
+tree.Add(3);
+tree.Add(1);
+tree.Add(5);
+tree.Add(4);
 
-console.log(tree);
-
-console.log("preOrder:", tree.preOrder());
-console.log("inOrder:", tree.inOrder());
-console.log("postOrder:", tree.postOrder());
-
-console.log("Contains 6:", tree.contains(6));
-console.log("Contains 9:", tree.contains(9));
+console.log(tree.findmin());
